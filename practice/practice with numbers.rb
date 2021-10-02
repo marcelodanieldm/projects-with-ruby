50538e76b5f971873d13e22f980ccace05f2b0bb
Marcelo Daniel <danielmarcelo83@gmail.com>
2021-10-02 17:57:27 -0300

    Create practice with numbers.rb

---
+++ practice/practice with numbers.rb
@@ -0,0 +1,17 @@
+#working with numbers, times and
+#random numbers
+puts "-"*20
+puts "I was a line"
+20.times{print "-"}
+20.times{puts "hi"}
+20.times{puts rand(10)}
+20.times{print "-"}
+#generating random numbers from 0 to 10
+puts rand(10)
+
+#float to integer and integer to float
+x=5.to_i
+puts x
+puts x.to_f
+y="10".to_f
+puts y
