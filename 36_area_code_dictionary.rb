104918b42f5b627d98b956beb342f60f70eb17d4
Marcelo Daniel <danielmarcelo83@gmail.com>
2021-10-05 20:24:14 -0300

    Update 36_area_code_dictionary.rb
    uploading the basic information

--- 36_area_code_dictionary.rb
+++ 36_area_code_dictionary.rb
@@ -1,14 +1,14 @@
 dial_book = {
-  "newyork" => "212",
-  "newbrunswick" => "732",
-  "edison" => "908",
-  "plainsboro" => "609",
-  "sanfrancisco" => "301",
-  "miami" => "305",
-  "paloalto" => "650",
-  "evanston" => "847",
-  "orlando" => "407",
-  "lancaster" => "717"
+  "laplata" => "221",
+  "cordoba" => "351",
+  "mendoza" => "261",
+  "mardelplata" => "223",
+  "buenosaires" => "011",
+  "riogallegos" => "227",
+  "neuquen" => "226",
+  "salta" => "332",
+  "ushuaia" => "220",
+  "posadas" => "375"
 }
  
 # Get city names from the hash
